66478e08396b4f38e89133d7e66c200ee37e7054
Shinn <sunikhedge@gmail.com>
2022-10-04 11:57:02 +0800

    updated scanner code file, shorten code further

--- VILORIA_CASTRO_LABACT2_SCANNER.java
+++ VILORIA_CASTRO_LABACT2_SCANNER.java
@@ -7,21 +7,16 @@ public class VILORIA_CASTRO_LABACT2_SCANNER {
         String lines = "===================================="; //for organizing
 
         //this starts the program
-        System.out.println(lines);
-        System.out.println("Welcome to Dizon Dairy Farms");
-        System.out.println(lines);
+        System.out.println(lines + "\nWelcome to Viloria & Castro Dairy Farms\n" + lines);
         System.out.print("Enter number of eggs: ");
         int amount = scan.nextInt(); 
-        System.out.println(lines);
-        System.out.printf("You ordered %,d eggs.\n", amount);
+        System.out.printf(lines + "\nYou ordered %,d eggs.\n", amount);
         int dozen = amount/12;
         int loose = amount%12;
         System.out.printf("No. of dozen: %,d at Php 36.00 per dozen.\n", dozen);
         System.out.printf("No. of loose eggs: %,d at Php 4.75 each.\n", loose);
-        System.out.println(lines);
         double total = (dozen * 36) + (loose * 4.75);
-        System.out.printf("Your total amount due is: Php %,.2f\n", total);
-        System.out.println(lines);
+        System.out.printf(lines + "\nYour total amount due is: Php %,.2f\n" + lines, total);
         scan.close();
     }
 
